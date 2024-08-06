7fbca362708a191ac9dcce0c0479ebb91f97c664
dqjbblp <hyisallpeople@gmail.com>
2024-08-06 16:43:30 +0800

    Add static resources

---
+++ tests/e2e/src/data/data.ts
@@ -0,0 +1,5 @@
+export enum NetworkSwitcher {
+  zkAmoebaEraCalibration = "/?network=calibration",
+  calibrationStage2 = "/?network=calibration-beta",
+  zkAmoebaEraMainnet = "/?network=mainnet",
+  }
