fcc61140036e526dd176350ce475d7f0ebb92305
dqjbblp <hyisallpeople@gmail.com>
2024-08-07 16:55:49 +0800

    Add configuration file

---
+++ src/configs/index.ts
@@ -0,0 +1,27 @@
+export type NetworkConfig = {
+  name: string;
+  icon: string;
+  verificationApiUrl?: string;
+  apiUrl: string;
+  rpcUrl: string;
+  bridgeUrl?: string;
+  l2NetworkName: string;
+  l2WalletUrl: string;
+  l2ChainId: 270 | 380 | 381;
+  l1ExplorerUrl?: string;
+  maintenance: boolean;
+  published: boolean;
+  hostnames: string[];
+  tokensMinLiquidity?: number;
+};
+
+export type EnvironmentConfig = {
+  networks: NetworkConfig[];
+};
+
+export type RuntimeConfig = {
+  version: string;
+  sentryDSN: string;
+  appEnvironment: string;
+  environmentConfig?: EnvironmentConfig;
+};
